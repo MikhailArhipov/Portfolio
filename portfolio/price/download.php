719d4c9bb1935cba679e57c7831fedb2269ccbf4
MikhailArhipov <162101498+MikhailArhipov@users.noreply.github.com>
2026-04-28 09:04:21 +0000

    Удалена папка portfolio

--- portfolio/price/download.php
+++
@@ -1,9 +0,0 @@
-<?
-include('pricescore.php');
-$exit = true;
-foreach ($checkFile as $el)
-    if (strpos($_GET['file'], $el) !== false) $exit = false;
-if ($exit) die;
-header('Content-type: plain/text');
-header('Content-Disposition: attachment; filename=' . $_GET['file']);
-readfile(dirname(__FILE__) . '/' . $_GET['file']);
